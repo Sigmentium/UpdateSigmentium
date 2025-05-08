8e507f1025a2498092e71753dc6e1c3c5d0e58d6
Markusha <legoanimation2022@gmail.com>
2025-05-08 11:00:12 +0300

    Update Main (1) (last)

--- index.js
+++ index.js
@@ -1,31 +1,3 @@
-document.getElementById('Table').addEventListener('click', function() {
+function Fall() {
     alert('Временно недоступно');
-});
-
-document.getElementById('Invest').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
-
-document.getElementById('Develop').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
-
-document.getElementById('Yrid').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
-
-document.getElementById('Table1').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
-
-document.getElementById('Invest1').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
-
-document.getElementById('Develop1').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
-
-document.getElementById('Yrid1').addEventListener('click', function() {
-    alert('Временно недоступно');
-});
+}
